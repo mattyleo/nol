d1c4b1feaffc94b535859fe491fa71fa630ac672
Mattia Leoni <mattialeoni@gmail.com>
2019-04-28 18:57:17 +0200

    Update main.js

--- data/js/main.js
+++ data/js/main.js
@@ -8,7 +8,6 @@ videoLoop.loop = true;
 videoLoop.muted = true;
 document.querySelector('.testata').appendChild(videoLoop);
 
-
 // Company Info 
 const heroContent = document.createElement('div');
 heroContent.className = 'hero-info';
@@ -25,60 +24,65 @@ description.innerHTML = 'Costa Smeralda watersports paradise';
 document.querySelector('.hero-info').appendChild(companyName);
 document.querySelector('.hero-info').appendChild(description);
 
-// set the video link
+// create video button
+const playButton = document.createElement('div');
+playButton.className = 'video-storage';
+document.querySelector('.hero-info').appendChild(playButton);
+const ytButton = document.createElement('img');
+ytButton.className = 'play-button';
+ytButton.src = 'data/images/ytlogo.png';
+document.querySelector('.video-storage').appendChild(ytButton);
 
-const surceV = '03Y4X7cG_EE/autoplay=1&enablejsapi=1';
-const surceVideo = 'https://www.youtube.com/embed/' + surceV;
-//const surceVideo = 'data/video/nolnautica.mp4';
-const linkVideo = surceVideo 
+// create the modal form
+const modal = document.createElement('div');
+modal.className = 'modal';
+document.querySelector('.hero-info').appendChild(modal);
 
-// modal central content
 
-const modalWindow = document.createElement('iframe');
-modalWindow.id = 'info-modal';
-modalWindow.className = 'modal';
-modalWindow.src = linkVideo;
-modalWindow.controls = true;
-document.querySelector('.hero-info').appendChild(modalWindow);
-
-// set the modal frame to play the video
-
-const videoData = document.createElement('div');
-videoData.className = 'video-storage';
-document.querySelector('.hero-info').appendChild(videoData);
-
-// crete the button to open the video file
-
-const play = 'data/images/play.png';
-const videoPlay = document.createElement('img');
-videoPlay.className = 'video-play';
-videoPlay.alt = 'youtube-play';
-videoPlay.src = play;
-document.querySelector('.video-storage').appendChild(videoPlay);
-
-//let-create the link
-const videoFile = document.createElement('a');
-videoFile.className = 'link-video';
-videoFile.setAttribute('href', '#');
-document.querySelector('.video-play').appendChild(videoFile);
-
-// play video in modal
-
-// const videoPresentation = document.createElement('video');
-// videoPresentation.className = 'nolnautica-video';
-// videoPresentation.src = linkVideo;
-// videoPresentation.controls = true;
-// document.querySelector('.modal').appendChild(videoPresentation);
-
-videoPlay.onclick =() => {
-  modalWindow.style.display = 'block';
+// create the player using YOUTUBE API
+let ytPlayer = document.createElement('div');
+ytPlayer.id = 'player';
+document.querySelector('.modal').appendChild(ytPlayer);
+const tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+let player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '360',
+    width: '640',
+    videoId: '03Y4X7cG_EE',
+    playerVars: { 'rel': 0},
+  });
 }
 
-// closing modal video session
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
 
+function pauseVideo() {
+  player.pauseVideo();
+}
+
+function playVideo() {
+  player.playVideo();
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
+
+// Show modal on click
+ytButton.onclick = () => {
+  modal.style.display = 'block';
+  player.playVideo();
+}
+
+// disable modal when user click outside the video
 window.onclick = () => {
-  if(event.target == modalWindow) {
-    modalWindow.style.display = 'none'
-    modalWindow.play(onclick) == false;
+  if(event.target == heroContent) {
+    modal.style.display = 'none';
+    player.stopVideo();
   }
 }
